bf329260f31b074f5e6c0edb7bf80dc7bb746187
Chaowalit Lalitpasan <chaowalitping@gmail.com>
2017-09-29 14:12:45 +0700

    Add description of MaximalAllowableSubArrays

--- src/Codefights/MaximalAllowableSubArrays.java
+++ src/Codefights/MaximalAllowableSubArrays.java
@@ -5,6 +5,33 @@ package Codefights;
  */
 public class MaximalAllowableSubArrays {
 
+//    Given an array of integers inputArray and an integer maxSum, for each i (0 ≤ i < length of inputArray) find the rightmost j (j < length of inputArray) such that the sum of all elements between the ith and jth elements (both inclusive) is not greater than maxSum.
+//
+//    Example
+//
+//    For inputArray = [2, 3, 0, 1, 2] and maxSum = 4, the output should be
+//    maximalAllowableSubarrays(inputArray, maxSum) = [0, 3, 4, 4, 4].
+//
+//    Input/Output
+//
+//  [time limit] 3000ms (java)
+//  [input] array.integer inputArray
+//
+//    A non-empty array of non-negative integers.
+//
+//    Guaranteed constraints:
+//            5 ≤ inputArray.length ≤ 104,
+//            0 ≤ inputArray[i] ≤ 106.
+//
+//            [input] integer maxSum
+//
+//    A non-negative integer not less than each of the array elements.
+//
+//    Guaranteed constraints:
+//    max(inputArray) ≤ maxSum ≤ 109.
+//
+//            [output] array.integer
+
     public static int[] maximalAllowableSubarrays(int[] r, int s) {
         int n = r.length;
 
